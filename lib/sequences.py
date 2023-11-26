9e0a3713a241aeb02ed8dfff96da9aa314acf359
Mikailalvares <mikailismail260@gmail.com>
2023-11-26 21:04:07 +0300

    final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return []
+    elif length == 1:
+        return [0]
+
+    fibonacci_sequence = [0, 1]
+    
+    while len(fibonacci_sequence) < length:
+        next_term = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_term)
+    
+    return fibonacci_sequence[:length]
